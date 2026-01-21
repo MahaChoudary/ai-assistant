d612ae5f1dfbe614e24a567f0f5dae3513c3feac
MAHEEN-HAMID <innovationswebwave@gmail.com>
2026-01-21 05:57:02 -0800

    env changed

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, request, jsonify
-from rag import get_answer
 from flask_cors import CORS
+from rag import get_answer
+import os
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/", methods=["GET"])
+def health():
+    return "AI backend is running"
+
 @app.route("/chat", methods=["POST"])
 def chat():
     data = request.get_json()
-    question = data["question"]
-
+    question = data.get("question", "")
     answer = get_answer(question)
-
     return jsonify({"answer": answer})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
